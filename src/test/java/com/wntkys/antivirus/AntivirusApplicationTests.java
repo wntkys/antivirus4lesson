3f22c3dbfad82326c404dc1f34ed16bfe5668029
wntkys <wntkys@ya.ru>
2024-09-27 22:43:07 +0300

    inital commit

---
+++ src/test/java/com/wntkys/antivirus/AntivirusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wntkys.antivirus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntivirusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
